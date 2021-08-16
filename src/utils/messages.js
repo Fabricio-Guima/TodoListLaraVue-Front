792cb6c3d3b5ff712d2a1525056e42392eec6be5
Fabricio-Guima <fsgkof@gmail.com>
2021-08-16 14:19:44 -0300

    teste
    teste

---
+++ src/utils/messages.js
@@ -0,0 +1,10 @@
+export default {
+    ServerError: "Ops! Alguma coisa deu errado.",
+    LoginInvalidException: "Ops! E-mail e/ou senha inválidos.",
+    UserHasBeenTakerException: "Ops! Parece que este usuário já existe.",
+    VerifyEmailTokenInvalidException:
+        "Ops! Parece que este token está inválido.",
+    UserNotFoundException: "Ops! Parece que este usuário não existe.",
+    ResetPasswordTokenInvalidException:
+        "Ops! Parece que este token está inválido.",
+};
